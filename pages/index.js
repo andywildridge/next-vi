3f3c0842869ed1e93ffe77fcf730e65b4649cfb7
andywildridge <andy.wildridge@gmail.com>
2021-05-12 21:08:02 +0100

    test deploy

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <p>test deploy</p>
+        {/*<h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
@@ -49,11 +50,12 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+  </div>*/}
       </main>
 
       <footer className={styles.footer}>
-        <a
+        <p>Footer</p>
+        {/*<a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -62,7 +64,7 @@ export default function Home() {
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </a>*/}
       </footer>
     </div>
   )
